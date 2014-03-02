03bfe0d2dde29d7a4a6192c291c1194af800e1da
cronosdead <alexmadden07@gmail.com>
2014-03-02 02:33:18 -0600

    Modificaciones, no implementa Bin2Str.

--- deco.py
+++ deco.py
@@ -1,9 +1,22 @@
 import Str2Bin as s2b
 import binascii
+import numpy as np
 
-s = s2b.Str2Bin('shit dos.')
-print s
+s = s2b.Str2Bin('ddshitdos.')
+n = np.array(s)
+n1 = np.transpose(n)
+(f, c) = n1.shape
+print n, n1
 cad = ''
-for i in range(len(s)):
-	cad = cad + s2b.Bin2Str(s[i])
-print cad
+aux = []
+for i in range(c):
+	for j in range(f):
+		cad = cad + str(n1[j][i])
+	aux.append(cad)
+	cad = ''
+print aux
+m = ''
+for i in range(len(aux)):
+	h = int(aux[i], 2)
+	m += binascii.unhexlify('%x' % h)
+print m
